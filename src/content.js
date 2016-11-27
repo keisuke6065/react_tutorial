8abd107f0947f9d87cf5574c6a3b4269c464ff0d
keisuke6065 <keisuke6065@gmail.com>
2016-11-27 18:41:33 +0900

    second commet

---
+++ src/content.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './index.css';
+
+class Li extends Component{
+  render() {
+    return (
+    <ul id="contet-ul">
+      <li>なんか</li>
+      <li>つくれるかな</li>
+    </ul>
+    );
+  }
+}
+
+
+export default Li;
