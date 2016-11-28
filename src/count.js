ad83041adf382f7ed24e01aadea8882dec3cae0c
中村 恵介 <keisuke.nakamura@bizreach.co.jp>
2016-11-28 17:34:12 +0900

    todo作成

---
+++ src/count.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class count extends Component {
+    render() {
+        return (
+            <div>
+                <p>count</p>
+            </div>
+        );
+    }
+}
+
+export default count;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import Li from './content';
+import Count from './count';
+import Todo from './todo';
 import './index.css';
 
 ReactDOM.render(
@@ -17,4 +19,12 @@ ReactDOM.render(
 ReactDOM.render(
   <Li />,
   document.getElementById('content')
+);
+ReactDOM.render(
+  <Count />,
+  document.getElementById('count')
+);
+ReactDOM.render(
+  <Todo />,
+  document.getElementById('todo')
 );
