eaab637064f3ca8623801da751131c69d6c6c7c5
中村 恵介 <keisuke.nakamura@bizreach.co.jp>
2016-12-07 10:03:16 +0900

    Component単位にまとめた

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Li from './content';
+import Count from './count';
+import Todo from './todo';
+import Timer from './timer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="test">
+        <Todo></Todo>
+        <Li></Li>
+        <Count></Count>
+        <Timer></Timer>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -1,35 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import Li from './content';
-import Count from './count';
-import Todo from './todo';
-import Timer from './timer';
 import './index.css';
 
 ReactDOM.render(
   <App />,
   document.getElementById('root')
-);
-
-ReactDOM.render(
-  <h1>title</h1>,
-  document.getElementById('sample')
-);
-
-ReactDOM.render(
-  <Li />,
-  document.getElementById('content')
-);
-ReactDOM.render(
-  <Count />,
-  document.getElementById('count')
-);
-ReactDOM.render(
-  <Todo />,
-  document.getElementById('todo')
-);
-ReactDOM.render(
-  <Timer />,
-  document.getElementById('timer')
 );
